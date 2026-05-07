b536f9b788b1205f50260f341079c783b4c49733
ikawaiimaha <kawaiimaha03@gmail.com>
2026-05-07 20:20:17 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from '@/components/Layout';
 import Home from '@/pages/Home';
 import CatalogPage from '@/pages/CatalogPage';
 import WardrobePage from '@/pages/WardrobePage';
@@ -14,22 +13,20 @@ import CreatorDashboard from '@/pages/CreatorDashboard';
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/wardrobe" element={<WardrobePage />} />
-          <Route path="/collection" element={<CollectionPage />} />
-          <Route path="/collection/:id" element={<CollectionPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/trader/:id" element={<ProfilePage />} />
-          <Route path="/offers" element={<OffersPage />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/creator" element={<CreatorDashboard />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/wardrobe" element={<WardrobePage />} />
+        <Route path="/collection" element={<CollectionPage />} />
+        <Route path="/collection/:id" element={<CollectionPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/trader/:id" element={<ProfilePage />} />
+        <Route path="/offers" element={<OffersPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/creator" element={<CreatorDashboard />} />
+      </Routes>
     </BrowserRouter>
   );
 }
