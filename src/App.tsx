ae8144732984409d2419bc8b929bf5dc91771c11
ikawaiimaha <kawaiimaha03@gmail.com>
2026-05-08 17:07:46 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
 import Home from './pages/Home';
 import CatalogPage from './pages/CatalogPage';
 import WardrobePage from './pages/WardrobePage';
 import ProfilePage from './pages/ProfilePage';
 import CreatorDashboard from './pages/CreatorDashboard';
+import NotificationsPage from './pages/NotificationsPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import BottomNav from './components/BottomNav';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-[#FDF8F7]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/wardrobe" element={<WardrobePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/creator" element={<CreatorDashboard />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-        <BottomNav /> 
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="min-h-screen bg-[#FDF8F7] dark:bg-[#1A0B2E] transition-colors duration-300">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/wardrobe" element={<WardrobePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/creator" element={<CreatorDashboard />} />
+            <Route path="/notifications" element={<NotificationsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+          <BottomNav /> 
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
