ed061b433549c9133885c880170d9d7882314668
ikawaiimaha <kawaiimaha03@gmail.com>
2026-05-12 22:26:52 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { AuthProvider } from './contexts/AuthContext' // Import the provider
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider> {/* Wrap App here */}
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
